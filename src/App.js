bd3e92830a6dcd34905e7da80566fe14b55de764
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-24 20:33:09 -0700

    Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>
+        May the force 💪 be with you
+      </h1>
+    </>
+  );
+}
+
+export default App;
