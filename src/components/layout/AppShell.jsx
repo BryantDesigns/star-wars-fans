f52724d44deb00c50e02bb9f8b50b5b3b357da02
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-25 18:00:02 -0700

    Added navbar and folder

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,12 +1,17 @@
-export default function Example() {
+import Navbar from "./navagation/Navbar";
+
+
+export default function AppShell() {
   return (
     <>
       <div className='min-h-full bg-gradient-to-tl from-gray-700 via-gray-900 to-black'>
         <div className='bg-[url("https://images.unsplash.com/photo-1532793218979-f3365c284b32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80")] bg-cover bg-center pb-32'>
-          {/* <Navbar /> */}
+          <Navbar />
           <header className='py-10'>
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-              <h1 className='text-3xl font-bold text-white'>Star Wars Characters</h1>
+              <h1 className='text-3xl font-bold text-white'>
+                Star Wars Characters
+              </h1>
             </div>
           </header>
         </div>
