1e5c7ed2812a2beeaabf632dcdf1c7594e157afb
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-25 17:37:41 -0700

    Add application shell and install dependencies

---
+++ src/components/layout/AppShell.jsx
@@ -0,0 +1,24 @@
+export default function Example() {
+  return (
+    <>
+      <div className='min-h-full bg-gradient-to-tl from-gray-700 via-gray-900 to-black'>
+        <div className='bg-[url("https://images.unsplash.com/photo-1532793218979-f3365c284b32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80")] bg-cover bg-center pb-32'>
+          {/* <Navbar /> */}
+          <header className='py-10'>
+            <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+              <h1 className='text-3xl font-bold text-white'>Star Wars Characters</h1>
+            </div>
+          </header>
+        </div>
+
+        <main className='-mt-32'>
+          <div className='mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8'>
+            <div className='rounded-lg bg-white px-5 py-6 shadow sm:px-6'>
+              {/* <Card /> */}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stardata: {
+          DEFAULT: "#9E4F60",
+          50: "#E1C3CA",
+          100: "#DAB6BE",
+          200: "#CD9BA5",
+          300: "#BF7F8D",
+          400: "#B26475",
+          500: "#9E4F60",
+          600: "#793C49",
+          700: "#532A33",
+          800: "#2E171C",
+          900: "#080405",
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
