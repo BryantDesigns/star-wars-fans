1e2baf2dc0ffd2fc60698b4ad753d0ba0e0288d5
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-26 19:12:11 -0700

    Search characters and debounce working

--- src/components/layout/navagation/Search.jsx
+++ src/components/layout/navagation/Search.jsx
@@ -1,6 +1,21 @@
+import { useRef } from "react";
+import { PEOPLE_SEARCH_URL } from "../../../constants/constants";
+import useDebounce from "../../../hooks/useDebounce";
 import { SearchIcon } from "@heroicons/react/solid";
 
-export default function Search() {
+const Search = ({ setSearchURL = () => {} }) => {
+  const { setSearchValue } = useDebounce(setSearchURL, 400);
+  const inputRef = useRef(null);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    if (!value) {
+      setSearchValue("");
+    } else {
+      setSearchValue(PEOPLE_SEARCH_URL + value);
+    }
+  };
+
   return (
     <div className='flex flex-1 justify-center px-2 lg:ml-6 lg:justify-end'>
       <div className='w-full max-w-lg lg:max-w-xs'>
@@ -17,12 +32,16 @@ export default function Search() {
           <input
             id='search'
             name='search'
+            type='search'
+            onChange={handleChange}
+            ref={inputRef}
             className='block w-full rounded-md border border-transparent bg-gray-800 py-2 pl-10 pr-3 leading-5 text-gray-400 placeholder-gray-400 focus:border-white focus:bg-stardata focus:text-white focus:outline-none focus:ring-white sm:text-sm'
             placeholder='Search Characters'
-            type='search'
           />
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Search;
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const PEOPLE_SEARCH_URL = "https://swapi.dev/api/people?search=";
