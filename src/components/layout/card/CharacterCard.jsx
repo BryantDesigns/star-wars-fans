e4a27a883d2552362e55e5324c0447ee061b2fe7
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-02-26 20:51:08 -0700

    update cloudinary url

--- src/components/layout/card/CharacterCard.jsx
+++ src/components/layout/card/CharacterCard.jsx
@@ -19,7 +19,7 @@ export default function CharacterCard({
     >
       <div className='min-h-80 lg:aspect-square aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-t-md bg-gray-200 group-hover:opacity-75 lg:h-80'>
         <img
-          src={`https://res.cloudinary.com/bryant-designs/image/upload/v1645922844/characters/${name}.jpg`}
+          src={`https://res.cloudinary.com/bryant-designs/image/upload/v1645922845/characters/${name}.jpg`}
           alt={name}
           className='h-full w-full object-cover object-center lg:h-full lg:w-full'
         />
